d96a717f51ff5085fdc08b890f194885490b8f6d
COREY MCCUE <coreymccue@COREYs-MacBook-Pro.local>
2020-12-29 15:49:09 +0100

    coded reset button functionality

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const p1Button = document.querySelector('#p1Button');
 const p2Button = document.querySelector('#p2Button');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const resetButton = document.querySelector('#reset')
 
 
 let p1Score = 0;
@@ -27,4 +28,12 @@ p2Button.addEventListener('click', function() {
         }
         p2Display.textContent = p2Score;
     }
+})
+
+resetButton.addEventListener('click', function() {
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
 })
